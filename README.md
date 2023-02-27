d23e0f5db13068e2192e66c58c9a70bd2d64a99c
licjavierbarrios <licjavierbarrios@hotmail.com>
2023-02-27 09:46:41 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Yeoman Maxy generator
+Yeoman generator for eslint and react things
+
+## Generators
+```
+yo maxy:dva
+yo maxy:eslint-node
+yo maxy:eslint-react
+yo maxy:eslint-svelte
+yo maxy:eslint-vue
+yo maxy:firebase-auth
+yo maxy:plop-react
+yo maxy:redux
+yo maxy:redux-saga
+yo maxy:redux-services
+yo maxy:root-provider
+yo maxy:root-routes
+yo maxy:snippets-react
+yo maxy:tailwind
+```
---
+++ generators/react/eslint/next/index.js
@@ -0,0 +1,28 @@
+const Generator = require("yeoman-generator");
+
+module.exports = class extends Generator {
+  installDependencies() {
+    this.spawnCommandSync("pnpm", [
+      "add",
+      "-D",
+      "prettier",
+      "eslint",
+      "eslint-config-prettier",
+      "eslint-config-standard",
+      "eslint-config-next",
+      "eslint-plugin-import",
+      "eslint-plugin-node",
+      "eslint-plugin-n",
+      "eslint-plugin-prettier",
+      "eslint-plugin-promise",
+      "eslint-plugin-react",
+      "eslint-plugin-react-hooks",
+      "@typescript-eslint/parser",
+      "@typescript-eslint/eslint-plugin",
+    ]);
+  }
+
+  eslint() {
+    this.fs.copy(this.templatePath(".eslintrc.json"), this.destinationPath(".eslintrc.json"));
+  }
+};
